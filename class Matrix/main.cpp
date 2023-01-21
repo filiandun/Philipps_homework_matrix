9d8e9dd187c93ae57948663153b0294f1e3d9475
filian <filiandun@gmail.com>
2023-01-21 23:20:50 +0300

    project: 100% ready

--- class Matrix/main.cpp
+++ class Matrix/main.cpp
@@ -1,7 +1,105 @@
 ﻿#include <iostream>
+#include <time.h>
+#include "matrix.h"
 
 int main()
 {
     setlocale(LC_ALL, "rus");
+    srand((unsigned int)time(0));
 
+    Matrix<int> matrix_1(4, 10, 100);
+    std::cout << "MATRIX 1: " << matrix_1;
+
+    Matrix<int> matrix_2(4, 10, 100);
+    std::cout << "MATRIX 2: " << matrix_2;
+
+
+// КОПИРУЮЩИЙ КОНСТРУКТОР
+    std::cout << std::endl << std::endl << "***********************************" << std::endl;
+    std::cout << "КОПИРУЮЩИЙ КОНСТРУКТОР" << std::endl;
+
+    Matrix<int> matrix_2_copy = matrix_2;
+    std::cout << "matrix 2: " << matrix_2;
+    std::cout << "matrix 2 copy: " << matrix_2_copy;
+
+
+// OPERATOR+
+    std::cout << std::endl << std::endl << "***********************************" << std::endl;
+    std::cout << "ОПЕРАТОР+" << std::endl;
+
+    Matrix<int> matrix_for_addition(matrix_1 + matrix_2);
+    std::cout << "matrix 1 + matrix 2: " << matrix_for_addition;
+
+// OPERATOR-
+    std::cout << std::endl << std::endl << "***********************************" << std::endl;
+    std::cout << "ОПЕРАТОР-" << std::endl;
+
+    Matrix<int> matrix_for_substraction(matrix_1 - matrix_2);
+    std::cout << "matrix 1 - matrix 2: " << matrix_for_substraction;
+
+// OPERATOR*
+    std::cout << std::endl << std::endl << "***********************************" << std::endl;
+    std::cout << "ОПЕРАТОР*" << std::endl;
+
+    Matrix<int> matrix_for_multiplication(matrix_1 * matrix_2);
+    std::cout << "matrix 1 * matrix 2: " << matrix_for_multiplication;
+
+// OPERATOR/
+    std::cout << std::endl << std::endl << "***********************************" << std::endl;
+    std::cout << "ОПЕРАТОР/" << std::endl;
+
+    Matrix<int> matrix_for_division(matrix_1 / matrix_2);
+    std::cout << "matrix 1 / matrix 2: " << matrix_for_division;
+
+
+// ФУНКЦИИ MIN() И MAX()
+    std::cout << std::endl << std::endl << "***********************************" << std::endl;
+    std::cout << "ФУНКЦИИ MIN() И MAX()" << std::endl;
+
+    std::cout << "минимальное число из matrix 1: " << matrix_1.min() << std::endl;
+    std::cout << "максимальное число из matrix 1: " << matrix_1.max() << std::endl << std::endl;
+
+
+// ОПЕРАТОР[]
+    std::cout << std::endl << std::endl << "***********************************" << std::endl;
+    std::cout << "ОПЕРАТОР[]" << std::endl;
+
+    Matrix<int> matrix_for_subscript(4, 10, 100);
+    std::cout << "matrix до оператора[]: " << matrix_for_subscript;
+
+    matrix_for_subscript[11] = 11;
+    matrix_for_subscript[12] = 12;
+    matrix_for_subscript[13] = 13;
+    matrix_for_subscript[14] = 14;
+
+    matrix_for_subscript[21] = 21;
+    matrix_for_subscript[22] = 22;
+    matrix_for_subscript[23] = 23;
+    matrix_for_subscript[24] = 24;
+
+    matrix_for_subscript[31] = 31;
+    matrix_for_subscript[32] = 32;
+    matrix_for_subscript[33] = 33;
+    matrix_for_subscript[34] = 34;
+
+    matrix_for_subscript[41] = 41;
+    matrix_for_subscript[42] = 42;
+    matrix_for_subscript[43] = 43;
+    matrix_for_subscript[44] = 44;
+
+    //matrix_3[52] = 100; // индекс вне диапазона
+
+    std::cout << "matrix после оператора[]: " << matrix_for_subscript;
+
+
+// ОПЕРАТОР>>
+    std::cout << std::endl << std::endl << "***********************************" << std::endl;
+    std::cout << "ОПЕРАТОР>>" << std::endl;
+
+    Matrix<int> matrix_for_input(2);
+    std::cin >> matrix_for_input;
+
+	std::cout << "MATRIX 3 ПОСЛЕ CIN: " << matrix_for_input;
+
+    return 0;
 }
--- class Matrix/matrix.h
+++ class Matrix/matrix.h
@@ -1,7 +1,270 @@
 #pragma once
+#include <iostream>
 
+
+template<typename T>
 class Matrix
 {
+public:
+	Matrix() // конструктор
+	{
+		this->size = 5;
 
+		this->array = new T *[this->size];
+		for (unsigned long long int i = 0; i < this->size; ++i)
+		{
+			this->array[i] = new T[this->size];
+		}
+
+		this->array[this->size][this->size] = '\0';
+	}
+
+
+	Matrix(unsigned long long int size) // конструктор с размером
+	{
+		this->size = size;
+
+		this->array = new T* [this->size];
+		for (unsigned long long int i = 0; i < this->size; ++i)
+		{
+			this->array[i] = new T [this->size];
+		}
+	}
+
+
+	Matrix(unsigned long long int size, unsigned long long int random_from, unsigned long long int random_to) // конструктор с рандомом
+	{
+		this->size = size;
+
+		this->array = new T * [this->size];
+		for (unsigned long long int i = 0; i < this->size; ++i)
+		{
+			this->array[i] = new T[this->size];
+		}
+
+
+		for (unsigned long long int i = 0; i < this->size; ++i)
+		{
+			for (unsigned long long int j = 0; j < this->size; ++j)
+			{
+				this->array[i][j] = rand() % (random_to - random_from) + random_from;
+			}
+		}
+	}
+
+
+	Matrix(const Matrix& matrix) // копирующий конструктор
+	{
+		this->size = matrix.size;
+
+		this->array = new T * [this->size];
+		for (unsigned long long int i = 0; i < this->size; ++i)
+		{
+			this->array[i] = new T[this->size];
+		}
+
+
+		for (unsigned long long int i = 0; i < this->size; ++i)
+		{
+			for (unsigned long long int j = 0; j < this->size; ++j)
+			{
+				this->array[i][j] = matrix.array[i][j];
+			}
+		}
+	}
+
+
+
+
+	~Matrix() // деструктор
+	{
+		for (unsigned long long int i = 0; i < this->size; ++i)
+		{
+			delete[] this->array[i];
+		}
+		delete[] this->array;
+	}
+
+
+private:
+	unsigned long long int size; // размер массива
+	T** array; // двухмерный массив
+
+
+public:
+	T min()
+	{
+		T min = this->array[0][0];
+
+		for (unsigned long long int i = 0; i < this->size; ++i)
+		{
+			for (unsigned long long int j = 0; j < this->size; ++j)
+			{
+				if (min > this->array[i][j])
+				{
+					min = this->array[i][j];
+				}
+			}
+		}
+
+		return min;
+	}
+
+	T max()
+	{
+		T max = this->array[0][0];
+
+		for (unsigned long long int i = 0; i < this->size; ++i)
+		{
+			for (unsigned long long int j = 0; j < this->size; ++j)
+			{
+				if (max < this->array[i][j])
+				{
+					max = this->array[i][j];
+				}
+			}
+		}
+
+		return max;
+	}
+
+
+
+
+
+
+	Matrix operator+(const Matrix& matrix)
+	{
+		Matrix addition(this->size);
+
+		for (unsigned long long int i = 0; i < this->size; ++i)
+		{
+			for (unsigned long long int j = 0; j < this->size; ++j)
+			{
+				addition.array[i][j] = this->array[i][j] + matrix.array[i][j];
+			}
+		}
+
+		return addition;
+	}
+
+
+	Matrix operator-(const Matrix& matrix)
+	{
+		Matrix substraction(this->size);
+
+		for (unsigned long long int i = 0; i < this->size; ++i)
+		{
+			for (unsigned long long int j = 0; j < this->size; ++j)
+			{
+				substraction.array[i][j] = this->array[i][j] - matrix.array[i][j];
+			}
+		}
+
+		return substraction;
+	}
+
+
+	Matrix operator*(const Matrix& matrix)
+	{
+		Matrix multiplictaion(this->size);
+
+		for (unsigned long long int i = 0; i < this->size; ++i)
+		{
+			for (unsigned long long int j = 0; j < this->size; ++j)
+			{
+				multiplictaion.array[i][j] = this->array[i][j] * matrix.array[i][j];
+			}
+		}
+
+		return multiplictaion;
+	}
+
+
+	Matrix operator/(const Matrix& matrix)
+	{
+		Matrix division(this->size);
+
+		for (unsigned long long int i = 0; i < this->size; ++i)
+		{
+			for (unsigned long long int j = 0; j < this->size; ++j)
+			{
+				division.array[i][j] = this->array[i][j] / matrix.array[i][j];
+			}
+		}
+
+		return division;
+	}
+
+
+
+
+
+
+	T& operator[](unsigned long long int index)
+	{
+		if (index / 10 - 1 < this->size)
+		{
+			return this->array[index / 10 - 1][index % 10 - 1]; // единственное, если матрица будет размером больше 10, то такой вариант не прокатит
+		}
+
+		std::cerr << "ОШИБКА: ИНДЕКС ВНЕ ДИАПАЗОНА" << std::endl;
+		exit(index);
+	}
+
+
+
+
+
+	friend std::ostream& operator<<(std::ostream& output, const Matrix& matrix)
+	{
+		output << std::endl << std::endl << "№";
+		for (unsigned long long int i = 0; i < matrix.size; ++i)
+		{
+			output << "\t" << i + 1;
+		}
+
+		for (unsigned long long int i = 0; i < matrix.size; ++i)
+		{
+			output << std::endl << std::endl << std::endl << i + 1 << "\t";
+			for (unsigned long long int j = 0; j < matrix.size; ++j)
+			{
+				output << matrix.array[i][j] << "\t";
+			}
+		}
+		output << std::endl << std::endl;
+
+		return output;
+	}
+
+
+	friend std::istream& operator>>(std::istream& input, Matrix& matrix)
+	{
+		matrix.array = new T* [matrix.size];
+		for (unsigned long long int i = 0; i < matrix.size; ++i)
+		{
+			matrix.array[i] = new T[matrix.size];
+		}
+
+		for (unsigned long long int i = 0; i < matrix.size; ++i)
+		{
+			for (unsigned long long int j = 0; j < matrix.size; ++j)
+			{
+				std::cout << "Введите элемент матрицы под индексами [" << i + 1 << "] [" << j + 1 << "]: ";
+				input >> matrix.array[i][j];
+
+				while (input.fail())
+				{
+					input.clear();
+					input.ignore();
+
+					std::cout << "Неверное значение, введите элемент матрицы под индексами [" << i + 1 << "] [" << j + 1 << "] повторно: ";
+					input >> matrix.array[i][j];
+				}
+			}
+		}
+		return input;
+	}
 };
+
 
